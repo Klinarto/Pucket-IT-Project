e87a7bced37a954bcab56f1b8ec3384a3c7c9abc
unknown <edberttantra@gmail.com>
2020-09-09 23:36:55 +1000

    Added paths for other required endpoints, refer to documentation in Confluence for expected use

--- server/index.js
+++ server/index.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/academic-experiences", (req, res) => {
+  res.send("Academic Experiences");
+});
+
+app.get("/hobbies", (req, res) => {
+  res.send("Academic Experiences");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
