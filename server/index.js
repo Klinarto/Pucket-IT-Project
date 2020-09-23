0ac3fa024b6b508724bfcef0072206299eab3201
unknown <edberttantra@gmail.com>
2020-09-24 04:49:31 +1000

    Working model, removed mongo.js since functionality will be used only once, pooling is done via MongoClient.connect() and req.app.db

--- server/index.js
+++ server/index.js
@@ -1,28 +1,71 @@
+const assert = require("assert");
+const fs = require("fs");
+
 const express = require("express");
 const app = express();
 const port = 5000;
 
-const mongo = require("./mongo.js");
-var db = mongo.db;
+const MongoClient = require("mongodb").MongoClient;
+
+//read json to get config properties, might change over to heroku environment variables
+fs.readFile("mongoProperties.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  dbProperties = JSON.parse(data);
+
+  //create new MongoClient with options
+  const client = new MongoClient(dbProperties.url, {
+    useNewUrlParser: true,
+    poolSize: dbProperties.poolSize,
+    useUnifiedTopology: true,
+  });
+
+  //connect client (must be done before using client to access DB)
+  client.connect((err) => {
+    if (err) {
+      console.error(err);
+    }
+
+    //specify DB to access
+    app.db = client.db(dbProperties.dbName);
+
+    //call expressJS to listen to port (now that DB is ready)
+    app.listen(port, () => {
+      console.log(`API site listening at http://localhost:${port}`);
+    });
+  });
+});
+
+/*
+ * ROUTES START HERE
+ */
 
 app.get("/api", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/api/academic-experiences", (req, res) => {
-  collection = db.collection("academicExperiences");
-  collection.insertOne({ a: 1 });
-  res.send("Academic Experiences");
+  collection = req.app.db.collection("academicExperience");
+  collection.find({}).toArray((err, docs) => {
+    if (err) {
+      throw err;
+    }
+    res.send(docs);
+  });
 });
 
 app.get("/api/hobbies", (req, res) => {
-  res.send("Academic Experiences");
+  collection = req.app.db.collection("hobbies");
+  collection.find({}).toArray((err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(data);
+  });
 });
 
 app.get("/api/contact", (req, res) => {
   res.send("Contact");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
