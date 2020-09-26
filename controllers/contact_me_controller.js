8ea96c5a92737642694ec1879511c87c82c5101c
unknown <edberttantra@gmail.com>
2020-09-27 00:31:28 +1000

    Added contact-me functionality

--- controllers/contact_me_controller.js
+++ controllers/contact_me_controller.js
@@ -1,27 +1,30 @@
-var nodemailer = require('nodemailer');
-const sendGridTransport = require('nodemailer-sendgrid-transport');
+const express = require("express");
+var nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
 
 var options = {
-    auth: {
-        api_key: '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'
-    }
-}
+  auth: {
+    api_key:
+      "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",
+  },
+};
 
 const mailer = nodemailer.createTransport(sendGridTransport(options));
 
-var sendContactMe = function(req, res){
-    var email = {
-        to: ['dimi3laksamana@gmail.com'],
-        from: 'pucket2020@gmail.com',
-        subject: 'E-Portfolio Contact',
-        html: '<b>Awesome sauce</b>'
-    };
-    mailer.sendMail(email, function(err, res) {
-        if (err) {
-            console.log(err)
-        }
-        console.log(res);
-    });
-}
+var sendContactMe = function (req, res) {
+  var email = {
+    to: ["lonekad729@debsmail.com"],
+    from: "pucket2020@gmail.com",
+    subject: "E-Portfolio Contact",
+    html: "<b>Awesome sauce</b>",
+  };
+  mailer.sendMail(email, function (err, res) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(res);
+  });
+  res.send("Message sent!");
+};
 
 module.exports.sendContactMe = sendContactMe;
