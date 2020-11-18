86e34893fc9b6affcd94168bde423713d5c5a02c
unknown <edberttantra@gmail.com>
2020-11-18 19:35:24 +1100

    Completed section test for CRUD operations, Also added authentication requirement for imgur token refresh

--- tests/section_test.js
+++ tests/section_test.js
@@ -52,13 +52,63 @@ describe('manipulating sections functionality', function () {
             expect(res.statusCode).to.equal(200);
             done();
         })
-        
-        // request(app)
-        //     .post('/admin/upload')
-        //     .set('Content-Type', 'application/json')
-        //     .set('x-auth-token', 'xyz123')
-        //     .send({section: "hobbies"})
-        //     .expect(200)
-        //     .end(done);
+    });
+
+    it("editing a section", function (done) {
+        var MockClient = mongodb.MongoClient;
+        MockClient.connect("mongodb://localhost:27017/", {}, function(err, client) {
+            var req = httpMocks.createRequest({
+                method: "POST",
+                path: "/admin/upload",
+                body: {
+                    section: "academic",
+                    title: "Test Entry",
+                    description : "Sample Test Entry",
+                    startDate: "2020-08-03",
+                    endDate: "2020-11-19",
+                    alignment: "left"
+                }
+            })
+            req.app = app;
+            app.db = client.db("MockDB");
+
+            var res = httpMocks.createResponse();
+            
+            admin.editEntry(req, res);
+            expect(res.statusCode).to.equal(200);
+            done();
+        })
+    });
+
+    it("deleting a section", function (done) {
+        var MockClient = mongodb.MongoClient;
+        MockClient.connect("mongodb://localhost:27017/", {}, function(err, client) {
+            var req = httpMocks.createRequest({
+                method: "POST",
+                path: "/admin/upload",
+                body: {
+                    section: "academic",
+                    _id: "5f9041aa313e99df10676cfa"
+                }
+            })
+            req.app = app;
+            app.db = client.db("MockDB");
+
+            var id = new mongodb.ObjectID("5f9041aa313e99df10676cfa");
+            app.db.collection("academic").insertOne({
+                _id: id,
+                title: "Test Entry",
+                startDate: "2020-08-03",
+                endDate: "2020-11-19",
+                description: "Sample Test Entry",
+                alignment: "left"
+            }).then((result) => {
+                var res = httpMocks.createResponse();
+            
+                admin.deleteEntry(req, res);
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
+        })
     });
 });
